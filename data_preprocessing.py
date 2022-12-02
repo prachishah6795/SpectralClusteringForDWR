bed029453109356ccde77b95f4570fe8e8fa869a
prachi.shah <72927441+prachishah6795@users.noreply.github.com>
2022-12-02 16:57:14 -0500

    Linking vars + cons decomposition working

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,7 +8,7 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import normalize
 import matplotlib.pylab as plt
 
-problem_set = ['fiber.mps.gz', 'timtab1.mps.gz', 'rout.mps.gz']
+problem_set = ['rout.mps.gz', 'fiber.mps.gz', 'timtab1.mps.gz']# , '10teams.mps.gz']
 
 miplib_dir = "MIPLIB 2003/"
 
@@ -76,9 +76,14 @@ class Instance:
         nodes = list(zip(non0_rows, non0_cols))
         self.G_row_col_net.add_nodes_from(nodes)
 
+        count_non_zeros_rows = np.count_nonzero(self.A, axis=1)
+        count_non_zeros_cols = np.count_nonzero(self.A, axis=0)
+
         for v1, v2 in itertools.combinations(nodes, 2):
-            if v1[0] == v2[0] or v1[1] == v2[1]:
-                self.G_row_col_net.add_edge(v1, v2)
+            if v1[0] == v2[0]:
+                self.G_row_col_net.add_edge(v1, v2, weight=1/count_non_zeros_rows[v1[0]])
+            if v1[1] == v2[1]:
+                self.G_row_col_net.add_edge(v1, v2, weight=1/count_non_zeros_cols[v1[1]])
 
         if len(list(nx.isolates(self.G_row_col_net))) > 0:
             warnings.warn("Graph contains isolated nodes")
@@ -123,19 +128,18 @@ class Instance:
             self.L[graph_type][laplacian_types[2]] = np.identity(D.shape[0]) - np.linalg.inv(D) @ W  # r-w
 
     def get_rearranged_matrix(self, labels, graph_type='row-net'):
+        assert graph_type in graph_types, 'check input'
 
         A_dwr = self.A.copy()
-        if graph_type == 'row-net':
+        if graph_type == graph_types[0]:
 
             labelled_nodes = np.array(sorted(zip(labels, range(self.n))))
-            # print(labelled_nodes)
             A_dwr = A_dwr[:, labelled_nodes[:, 1]]  # rearranging columns by clusters
             A_abs_sum_rows = np.sum(np.abs(self.A), axis=1).flatten()
 
             # Rearrange rows
             unique_labels, unique_indices = np.unique(labelled_nodes[:, 0], return_index=True)
             var_clusters = np.split(labelled_nodes[:, 1], unique_indices[1:])
-            # print(var_clusters)
 
             row_cluster = []
             for i in range(len(unique_labels)):
@@ -153,31 +157,60 @@ class Instance:
             for each in row_cluster:
                 print(each.shape, end=', ')
                 row_order.extend(each)
-            print()
 
             linking_rows = set(range(self.m)) - set(row_order)
             row_order.extend(linking_rows)
+            print(len(linking_rows))
 
             A_dwr = A_dwr[row_order, :]
+
+        if graph_type == graph_types[1]:
+            labelled_nodes = np.array(sorted(zip(labels, self.G_row_col_net.nodes)), dtype=object)
+            unique_labels, unique_indices = np.unique(labelled_nodes[:, 0], return_index=True)
+            coef_clusters = np.split(labelled_nodes[:, 1], unique_indices[1:])
+
+            cons_clusters, var_clusters = [], []
+            for cluster in coef_clusters:
+                cons, vars = zip(*cluster)
+                cons_clusters.append(np.unique(np.fromiter(cons, dtype=int)))
+                var_clusters.append(np.unique(np.fromiter(vars, dtype=int)))
+
+            linking_vars = set(v for v in range(self.n) if sum([v in cl for cl in var_clusters]) >= 2)
+            linking_cons = set(c for c in range(self.m) if sum([c in cl for cl in cons_clusters]) >= 2)
+
+            col_order = []
+            for cl in var_clusters:
+                col_order.extend(set(cl) - linking_vars)
+                print(len(set(cl) - linking_vars), end=', ')
+            col_order.extend(linking_vars)
+            print(len(linking_vars))
+
+            row_order = []
+            for cl in cons_clusters:
+                row_order.extend(set(cl) - linking_cons)
+                print(len(set(cl) - linking_cons), end=', ')
+            row_order.extend(linking_cons)
+            print(len(linking_cons))
+
+            A_dwr = A_dwr[row_order, :]
+            A_dwr = A_dwr[:, col_order]
 
         return A_dwr
 
 
 
 out = {}
-for problem in problem_set[:1]:
+for problem, k in zip(problem_set, [4, 3, 2]):
     instance = Instance(problem)
     print(f"matrix size = {instance.m}, {instance.n}")
     init_matrix = instance.A
-    out[problem] = instance.spectral_clustering(graph_types[0], laplacian_types[1], K=4)
+    out[problem] = instance.spectral_clustering(graph_types[1], laplacian_types[1], K=k)
     rearranged_matrix = out[problem]
 
-    plt.spy(init_matrix)
+    plt.spy(init_matrix, markersize=1)
     plt.show()
 
-    plt.spy(rearranged_matrix)
+    plt.spy(rearranged_matrix, markersize=1)
     plt.show()
-
-
 
 
